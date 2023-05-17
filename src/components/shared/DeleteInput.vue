41abb32dcf90226365813067616335aa951c4492
Artem Lemelson <lemelson@Artems-Laptop.local>
2023-05-17 03:36:21 +0300

    First commit

---
+++ src/components/shared/DeleteInput.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="button-container align-center flex">
+    <v-text-field
+      v-if="!onlypop"
+      color="error"
+      class="text-field"
+      placeholder="Удалить число"
+      :value="deletedValue"
+      @input="deletedValue = $event"
+      @keyup.enter="deleteValue"
+    />
+    <v-btn
+      color="error"
+      class="button"
+      :disabled="disabled"
+      @click="deleteValue"
+    >
+      удалить
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DeleteInput',
+  props: {
+    onlypop: { type: Boolean },
+    disabled: { type: Boolean },
+  },
+  data() {
+    return {
+      deletedValue: '',
+    };
+  },
+  methods: {
+    deleteValue() {
+      this.$emit('myEvent', this.deletedValue);
+      this.deletedValue = '';
+    },
+  },
+};
+</script>
+
+<style>
+.button-container {
+  width: 100%;
+  max-width: 300px;
+}
+.text-field {
+  margin-right: 1rem;
+}
+.button {
+  min-width: fit-content !important;
+}
+</style>
---
+++ src/components/shared/Header.vue
@@ -0,0 +1,30 @@
+<template fluid>
+  <v-card
+    color="#edf2f7"
+    flat
+    tile
+    height="80px"
+  />
+</template>
+
+<script>
+
+import Router from 'vue-router';
+
+const router = new Router();
+
+export default {
+  name: 'Header',
+  methods: {
+    goBack() {
+      router.go(-1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header-logo {
+  max-width: 120px;
+}
+</style>
